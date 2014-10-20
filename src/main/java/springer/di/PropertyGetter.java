ae8f5eed6e68485c42efbf39e4d12b36ebb6e47d
Shantanu Kumar <kumar.shantanu@gmail.com>
2014-10-21 00:17:31 +0530

    add javadoc and coercions to PropertyGetter

--- src/main/java/springer/di/PropertyGetter.java
+++ src/main/java/springer/di/PropertyGetter.java
@@ -1,43 +1,138 @@
 package springer.di;
 
-import java.util.Properties;
+import java.util.Map;
 
+/**
+ * Utility class to easily create component source from a key-value map.
+ *
+ * @param <K> component key type in dependency graph
+ */
 public class PropertyGetter<K> {
 
-    private final Properties properties;
+    private final Map<?, ?> properties;
     private final Class<K> componentKeyClass;
 
-    public PropertyGetter(Properties properties, Class<K> componentKeyClass) {
+    /**
+     * Construct a <tt>PropertyGetter</tt> instance from a map of property key-value pairs and component key type.
+     * @param properties a map of key-value pairs
+     * @param componentKeyClass component type
+     */
+    public PropertyGetter(Map<?, ?> properties, Class<K> componentKeyClass) {
         this.properties = properties;
         this.componentKeyClass = componentKeyClass;
     }
 
-    public IComponentSource<Object, K> get(String name) {
-        return DI.constantly(properties.get(name), componentKeyClass);
+    /**
+     * Given a property key, retrieve the corresponding value and return a component source wrapper of the value.
+     * @param key property key
+     * @return component source that always returns the corresponding property value
+     */
+    public IComponentSource<?, K> get(Object key) {
+        return DI.constantly(properties.get(key), componentKeyClass);
     }
 
-    public IComponentSource<String, K> getString(String name) {
-        return DI.constantly((String) properties.get(name), componentKeyClass);
+    /**
+     * Given a property key, retrieve the corresponding <tt>String</tt> value and return a component source wrapper of
+     * the value.
+     * @param key property key
+     * @return component source that always returns the corresponding property value as <tt>String</tt>
+     */
+    public IComponentSource<String, K> getString(Object key) {
+        final Object value = properties.get(key);
+        if (value instanceof String || value == null) {
+            return DI.constantly((String) value, componentKeyClass);
+        }
+        return DI.constantly(value.toString(), componentKeyClass);
     }
 
-    public IComponentSource<Boolean, K> getBoolean(String name) {
-        return DI.constantly(Boolean.parseBoolean((String) properties.get(name)), componentKeyClass);
+    /**
+     * Given a property key, retrieve the corresponding <tt>Boolean</tt> value and return a component source wrapper of
+     * the value.
+     * @param key property key
+     * @return component source that always returns the corresponding property value as <tt>Boolean</tt>
+     */
+    public IComponentSource<Boolean, K> getBoolean(Object key) {
+        final Object value = properties.get(key);
+        if (value instanceof Boolean) {
+            return DI.constantly((Boolean) value, componentKeyClass);
+        }
+        if (value instanceof String) {
+            return DI.constantly(Boolean.parseBoolean((String) value), componentKeyClass);
+        }
+        throw new IllegalArgumentException(String.format(
+                "Corresponding value for key '%s' can not be parsed as Boolean: %s", key, value));
     }
 
-    public IComponentSource<Integer, K> getInteger(String name) {
-        return DI.constantly(Integer.parseInt((String) properties.get(name)), componentKeyClass);
+    /**
+     * Given a property key, retrieve the corresponding <tt>Integer</tt> value and return a component source wrapper of
+     * the value.
+     * @param key property key
+     * @return component source that always returns the corresponding property value as <tt>Integer</tt>
+     */
+    public IComponentSource<Integer, K> getInteger(Object key) {
+        final Object value = properties.get(key);
+        if (value instanceof Number) {
+            return DI.constantly(((Number) value).intValue(), componentKeyClass);
+        }
+        if (value instanceof String) {
+            return DI.constantly(Integer.parseInt((String) value), componentKeyClass);
+        }
+        throw new IllegalArgumentException(String.format(
+                "Corresponding value for key '%s' can not be parsed as Integer: %s", key, value));
     }
 
-    public IComponentSource<Long, K> getLong(String name) {
-        return DI.constantly(Long.parseLong((String) properties.get(name)), componentKeyClass);
+    /**
+     * Given a property key, retrieve the corresponding <tt>Long</tt> value and return a component source wrapper of
+     * the value.
+     * @param key property key
+     * @return component source that always returns the corresponding property value as <tt>Long</tt>
+     */
+    public IComponentSource<Long, K> getLong(Object key) {
+        final Object value = properties.get(key);
+        if (value instanceof Number) {
+            return DI.constantly(((Number) value).longValue(), componentKeyClass);
+        }
+        if (value instanceof String) {
+            return DI.constantly(Long.parseLong((String) value), componentKeyClass);
+        }
+        throw new IllegalArgumentException(String.format(
+                "Corresponding value for key '%s' can not be parsed as Long: %s", key, value));
     }
 
-    public IComponentSource<Float, K> getFloat(String name) {
-        return DI.constantly(Float.parseFloat((String) properties.get(name)), componentKeyClass);
+    /**
+     * Given a property key, retrieve the corresponding <tt>Float</tt> value and return a component source wrapper of
+     * the value.
+     * @param key property key
+     * @return component source that always returns the corresponding property value as <tt>Float</tt>
+     */
+    public IComponentSource<Float, K> getFloat(Object key) {
+        final Object value = properties.get(key);
+        if (value instanceof Number) {
+            return DI.constantly(((Number) properties.get(key)).floatValue(), componentKeyClass);
+        }
+        if (value instanceof String) {
+            return DI.constantly(Float.parseFloat((String) value), componentKeyClass);
+        }
+        throw new IllegalArgumentException(String.format(
+                "Corresponding value for key '%s' can not be parsed as Float: %s", key, value));
     }
 
-    public IComponentSource<Double, K> getDouble(String name) {
-        return DI.constantly(Double.parseDouble((String) properties.get(name)), componentKeyClass);
+    /**
+     * Given a property key, retrieve the corresponding <tt>Double</tt> value and return a component source wrapper of
+     * the value.
+     * @param key property key
+     * @return component source that always returns the corresponding property value as <tt>Double</tt>
+     */
+    public IComponentSource<Double, K> getDouble(Object key) {
+        final Object value = properties.get(key);
+        if (value instanceof Number) {
+            return DI.constantly(((Number) value).doubleValue(), componentKeyClass);
+        }
+        if (value instanceof String) {
+            return DI.constantly(Double.parseDouble((String) value), componentKeyClass);
+        }
+        throw new IllegalArgumentException(String.format(
+                "Corresponding value for key '%s' can not be parsed as Double: %s", key, value));
     }
 
 }
