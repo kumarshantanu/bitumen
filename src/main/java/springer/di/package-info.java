658f331d3350db79239e5d6d80e071a3d5fa07d1
Shantanu Kumar <kumar.shantanu@gmail.com>
2014-10-25 13:03:27 +0530

    fix javadoc reference to classes

--- src/main/java/springer/di/package-info.java
+++ src/main/java/springer/di/package-info.java
@@ -1,8 +1,8 @@
 /**
  * <h3>Springer Dependency-Injection Support</h3>
  *
- * Springer DI's support is built on the fundamental abstraction {@link IComponentSource}. It is used in the dependency
- * map in {@link DependencyBuilder} so as to resolve transitive dependencies and form components. The {@link DI} class
- * holds a bunch of static utility methods to create various kinds of component sources.
+ * Springer DI's support is built on the fundamental abstraction {@link springer.di.IComponentSource}. It is used in the
+ * dependency map in {@link springer.di.DependencyBuilder} so as to resolve transitive dependencies and form components.
+ * The {@link springer.di.DI} class has a bunch of static utility methods to create various kinds of component sources.
  */
 package springer.di;
